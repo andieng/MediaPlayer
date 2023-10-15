c7d1c2753ce6ece9a48a2fc0cc6f44a70a0ff9be
Nguyen Ngoc Thuy <thuy.nguyen@bizzi.vn>
2023-10-15 16:25:42 +0700

    Add project files.

---
+++ MediaPlayer/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+
+namespace MediaPlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Media> mediaList = new List<Media>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string img = "Images/musical-note-64x64.png";
+            string img2 = "Images/film-64x64.png";
+
+            string currentDir = AppDomain.CurrentDomain.BaseDirectory;
+            mediaList.Add(new Media(currentDir + "cruel-summer1.mp3", 644000.45, img));
+            mediaList.Add(new Media(currentDir + "cruel-summer2.mp3", 100000.5,img2));
+            mediaList.Add(new Media(currentDir + "cruel-summer3.mp3", 6000.8, img));
+            mediaList.Add(new Media(currentDir + "cruel-summer4.mp3", 279.25, img2));
+            mediaList.Add(new Media(currentDir + "cruel-summer5.mp3", 103000.6, img2));
+            mediaList.Add(new Media(currentDir + "cruel-summer6.mp3", 644000.45, img));
+            mediaList.Add(new Media(currentDir + "cruel-summer7.mp3", 100000.5, img2));
+            mediaList.Add(new Media(currentDir + "cruel-summer8.mp3", 6000.8, img));
+            mediaList.Add(new Media(currentDir + "cruel-summer9.mp3", 279.25, img2));
+            mediaList.Add(new Media(currentDir + "cruel-summer10.mp3", 103000.6, img2));
+            mediaList.Add(new Media(currentDir + "cruel-summer11.mp3", 644000.45, img));
+            mediaList.Add(new Media(currentDir + "cruel-summer12.mp3", 644000.45, img));
+            plListView.ItemsSource = mediaList;
+        }
+    }
+}
---
+++ MediaPlayer/Media.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+//using System.IO;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace MediaPlayer
+{
+    class Media : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string? FilePath { get; set; }
+        public ImageSource? PreviewImage { get; set; }
+        public string? Name
+        {
+            get => Path.GetFileNameWithoutExtension(FilePath);
+        }
+        public double Duration { get; set; }
+        public string DurationString 
+        { 
+            get => TimeSpan.FromSeconds(Duration).ToString(@"hh\:mm\:ss");
+        }
+
+        public Media(string? filePath, double duration, ImageSource? previewImage)
+        {
+            this.FilePath = filePath;
+            this.Duration = duration;
+            this.PreviewImage = previewImage;
+        }
+
+        public Media(string? filePath, double duration, string? previewImage)
+        {
+            this.FilePath = filePath;
+            this.Duration = duration;
+            string workDir = Environment.CurrentDirectory;
+            string projectDir = Directory.GetParent(workDir).Parent.Parent.FullName;
+            Uri uri = new Uri($"{projectDir}/{previewImage}", UriKind.Absolute);
+            this.PreviewImage = new BitmapImage(uri);
+        }
+    }
+}
