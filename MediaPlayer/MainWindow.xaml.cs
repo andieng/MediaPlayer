f254e0a8945855381b44aeaaff3e3a2ff000ca81
Thanh <ntnthanh.109@gmail.com>
2023-10-21 10:39:06 +0700

    #load and save

--- MediaPlayer/MainWindow.xaml.cs
+++ MediaPlayer/MainWindow.xaml.cs
@@ -7,12 +7,16 @@ using WMPLib;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows.Controls;
+using Path = System.IO.Path;
+using System.Net;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace MediaPlayer
 {
     public partial class MainWindow : Window
     {
         private ObservableCollection<Media> mediaList = new ObservableCollection<Media>();
+        private bool hasMediaListBeenSaved = false;
         string thumbnail_audio = "Images/musical-note-64x64.png";
         string thumbnail_video = "Images/film-64x64.png";
         public MainWindow()
@@ -83,5 +87,100 @@ namespace MediaPlayer
         {
             MessageBox.Show("detail");
         }
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            var folderDialog = new CommonOpenFileDialog
+            {
+                IsFolderPicker = true,
+                Title = "Select a Folder"
+            };
+
+            if (folderDialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                string selectedFolderPath = folderDialog.FileName;
+                string newFolderName = "MyMediaFolder";
+                string newFolderPath = Path.Combine(selectedFolderPath, newFolderName);
+
+                if (!Directory.Exists(newFolderPath))
+                {
+                    Directory.CreateDirectory(newFolderPath);
+                }
+                foreach (var media in mediaList)
+                {
+                    string fileName = Path.GetFileName(media.FilePath);
+                    string filePath = Path.Combine(newFolderPath, fileName);
+                    WebClient webClient = new WebClient();
+                    try
+                    {
+                        webClient.DownloadFile(media.Source.AbsoluteUri, filePath);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error downloading file: {ex.Message}");
+                    }
+                }
+                mediaList.Clear();
+                hasMediaListBeenSaved = true;
+            }
+
+
+              
+        }
+
+
+
+
+        private void addListPathFile(string[] selectedFilePaths)
+        {
+       
+
+                foreach (string selectedFilePath in selectedFilePaths)
+                {
+                    var player = new WindowsMediaPlayer();
+                    var clip = player.newMedia(selectedFilePath);
+
+                    string extension = Path.GetExtension(selectedFilePath).ToLower();
+
+                    bool fileExists = mediaList.Any(media => media.FilePath == selectedFilePath);
+
+                    if (!fileExists)
+                    {
+                        if (extension == ".mp3" || extension == ".flac" || extension == ".ogg" || extension == ".wav")
+                        {
+                            mediaList.Add(new Media(selectedFilePath, clip.duration, thumbnail_audio));
+                        }
+                        else if (extension == ".mp4" || extension == ".avi" || extension == ".mkv")
+                        {
+                            mediaList.Add(new Media(selectedFilePath, clip.duration, thumbnail_video));
+                        }
+                        else
+                        {
+                            // Xử lý ngoại lệ
+                        }
+                    }
+                }
+        }
+        private void importButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!hasMediaListBeenSaved)
+            {
+                MessageBox.Show("Please save your media files before loading new ones.", "Alert", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            var folderDialog = new CommonOpenFileDialog
+            {
+                IsFolderPicker = true,
+                Title = "Select a Folder"
+            };
+
+            if (folderDialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                string selectedFolderPath = folderDialog.FileName;
+                string[] files = Directory.GetFiles(selectedFolderPath, "*", SearchOption.AllDirectories);
+                addListPathFile(files);
+            }
+            hasMediaListBeenSaved = true;
+        }
+
     }
 }
